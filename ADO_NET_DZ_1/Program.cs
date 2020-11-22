ed76a0b06b27c7be08d04a982d70c721ea433966
Chmil Ruslan <chmilrv@users.noreply.github.com>
2020-11-22 20:30:49 +0200

    2030

--- ADO_NET_DZ_1/Program.cs
+++ ADO_NET_DZ_1/Program.cs
@@ -18,28 +18,58 @@ namespace ADO_NET_DZ_1
         public Program()
         {
             connection = new SqlConnection();
-
-            // Предыдущий вариант использования строки подключения
-
-            // conn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;
-            //    Initial Catalog=Library;
-            //    Integrated Security=SSPI;";
-
-            // Для доступа к файлу конфигурации приложения из нашего кода
-            // мы будем пользоваться классом ConfigurationManager,
-            // определенным в пространстве имен System.Configuration.
-
             connection.ConnectionString = ConfigurationManager.ConnectionStrings["ConnectionString_DZ1"].ConnectionString;
         }
 
 
+        public void ReadData()
+        {
+            int countBooks = 0;
+            SqlDataReader dataReader = null;
+            try
+            {
+                connection.Open();
+                string sqlQuery_1 = "Select count(id) from Books;";
+                SqlCommand sqlCommand_1 = new SqlCommand(sqlQuery_1,connection);
+                countBooks = (Int32)sqlCommand_1.ExecuteScalar();
+                //WriteLine($"Количество книг {countBooks}");
+                string sqlQuery_2 = "Select Title, PRICE, PAGES from Books;";
+                SqlCommand sqlCommand_2 = new SqlCommand(sqlQuery_2, connection);
+                dataReader = sqlCommand_2.ExecuteReader();
+                bool headLine = true;
+                int pagesSumm = 0;
+                int priceSumm = 0;
+                //for (int i = 0; i < countBooks; i++)
+                while (dataReader.Read())
+                {
+                    if (headLine)
+                    {
+                        for (int j = 0; j < dataReader.FieldCount; j++) Write(dataReader.GetName(j).ToString() + "\t\t\t");
+                        WriteLine();
+                    }
+                    headLine = false;
+                    string title = dataReader.GetString(0);
+                    int price = dataReader.GetInt32(1);
+                    int pages = dataReader.GetInt32(2);
+                    priceSumm += price;
+                    pagesSumm += pages;
+                    WriteLine(title + "\t" + price + "\t\t\t" + pages);
+                }
+                WriteLine($"Сумарная цена всех книг: {priceSumm}.");
+                WriteLine($"Сумарное количество страниц всех книг: {pagesSumm}.");
+            }
+            finally
+            {
+                if (dataReader != null) dataReader.Close();
+                if (connection != null) connection.Close();
+            }
+
+        }
 
         static void Main(string[] args)
         {
-
-
-
-
+            Program pr = new Program();
+            pr.ReadData();
             ReadKey();
         }
     }
