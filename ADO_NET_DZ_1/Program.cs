d473bbf014b1ed2ddd2f29a9451c436d2cf6a217
Chmil Ruslan <chmilrv@users.noreply.github.com>
2020-11-22 20:57:01 +0200

    2056

--- ADO_NET_DZ_1/Program.cs
+++ ADO_NET_DZ_1/Program.cs
@@ -33,14 +33,19 @@ namespace ADO_NET_DZ_1
                 SqlCommand sqlCommand_1 = new SqlCommand(sqlQuery_1,connection);
                 countBooks = (Int32)sqlCommand_1.ExecuteScalar();
                 //WriteLine($"Количество книг {countBooks}");
-                string sqlQuery_2 = "Select Title, PRICE, PAGES from Books;";
+                string sqlQuery_2 = "Select B.Title as Title," +
+                    "                       A.FirstName + ' ' + A.LastName as Author," +
+                    "                       B.PRICE as Price," +
+                    "                       B.PAGES as Pages" +
+                    "                from Books as B" +
+                    "                join Authors as A on B.AuthorId=A.Id;";
                 SqlCommand sqlCommand_2 = new SqlCommand(sqlQuery_2, connection);
                 dataReader = sqlCommand_2.ExecuteReader();
                 bool headLine = true;
                 int pagesSumm = 0;
                 int priceSumm = 0;
-                //for (int i = 0; i < countBooks; i++)
-                while (dataReader.Read())
+                Write("#\t");
+                for (int i = 0; i < countBooks; i++)
                 {
                     if (headLine)
                     {
@@ -48,14 +53,16 @@ namespace ADO_NET_DZ_1
                         WriteLine();
                     }
                     headLine = false;
+                    dataReader.Read();
                     string title = dataReader.GetString(0);
-                    int price = dataReader.GetInt32(1);
-                    int pages = dataReader.GetInt32(2);
+                    string author = dataReader.GetString(1);
+                    int price = dataReader.GetInt32(2);
+                    int pages = dataReader.GetInt32(3);
                     priceSumm += price;
                     pagesSumm += pages;
-                    WriteLine(title + "\t" + price + "\t\t\t" + pages);
+                    WriteLine((i + 1) + "\t" + title + "\t" + author + "\t\t" + price + "\t\t\t" + pages);
                 }
-                WriteLine($"Сумарная цена всех книг: {priceSumm}.");
+                WriteLine($"\nСумарная цена всех книг: {priceSumm}.");
                 WriteLine($"Сумарное количество страниц всех книг: {pagesSumm}.");
             }
             finally
@@ -63,11 +70,10 @@ namespace ADO_NET_DZ_1
                 if (dataReader != null) dataReader.Close();
                 if (connection != null) connection.Close();
             }
-
         }
-
         static void Main(string[] args)
         {
+            Title = "ADO_NET_DZ_1";
             Program pr = new Program();
             pr.ReadData();
             ReadKey();
