8243f123fede98b28c7ecc6b8147b7c87114f44f
Chmil Ruslan <chmilrv@users.noreply.github.com>
2020-11-22 19:18:50 +0200

    Добавьте файлы проекта.

---
+++ ADO_NET_DZ_1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ADO_NET_DZ_1
+{
+
+
+
+    class Program
+    {
+        SqlConnection connection = null;
+        public Program()
+        {
+            connection = new SqlConnection();
+
+            // Предыдущий вариант использования строки подключения
+
+            // conn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;
+            //    Initial Catalog=Library;
+            //    Integrated Security=SSPI;";
+
+            // Для доступа к файлу конфигурации приложения из нашего кода
+            // мы будем пользоваться классом ConfigurationManager,
+            // определенным в пространстве имен System.Configuration.
+
+            connection.ConnectionString = ConfigurationManager.ConnectionStrings["ConnectionString_DZ1"].ConnectionString;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+
+
+
+
+            ReadKey();
+        }
+    }
+}
---
+++ ADO_NET_Lesson_01_DZ.sql
@@ -0,0 +1,35 @@
+﻿use [master];
+go
+create database [Library];
+go
+use [Library];
+go
+CREATE TABLE [dbo].[Authors]
+(
+	Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	FirstName VARCHAR(100) NOT NULL,
+	LastName VARCHAR(100) NOT NULL
+)
+GO
+CREATE TABLE [dbo].[Books]
+(
+	Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	AuthorId INT NOT NULL,
+	FOREIGN KEY (AuthorId) REFERENCES AUTHORS (Id),
+	Title VARCHAR(100) NOT NULL,
+	PRICE INT,
+	PAGES INT
+)
+GO
+INSERT INTO Authors (FirstName, LastName) VALUES
+	('Isaac', 'Asimov'),
+	('Roger', 'Zelazny');
+GO
+INSERT INTO Books (AuthorId, Title, Price, Pages) VALUES
+	(1, 'Pebble in the Sky', 250, 220),
+	(1, 'The Stars, Like Dust', 340, 280),
+	(1, 'The Currents of Space', 180, 200),
+	(2, 'Nine Princes in Amber', 440, 320),
+	(2, 'The Guns of Avalon', 390, 410),
+	(2, 'Sign of the Unicorn', 330, 380);
+GO
